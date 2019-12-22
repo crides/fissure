2e97013a39cfecc7b9c06be2723159a0b8a2d225
crides <zhuhaoqing@live.cn>
2019-12-21 22:39:39 -0600

    update keymaps

--- qmk/keymaps/5plit/keymap.c
+++ qmk/keymaps/5plit/keymap.c
@@ -30,11 +30,10 @@ enum combos {
 
 // Layers
 #define _BASE   0
-#define _STENO  1
-#define _GAME   2
-#define _SYMBOL1 3
-#define _SYMBOL2 4
-#define _FUNC 5
+#define _GAME   1
+#define _SYMBOL1 2
+#define _SYMBOL2 3
+#define _FUNC 4
 
 #define KEYMAP( \
     L10, L11, L12, L13, L14, 				R10, R11, R12, R13, R14, \
@@ -76,11 +75,6 @@ void suspend_wakeup_init_user() {
 
 layer_state_t layer_state_set_user(layer_state_t state) {
     switch (biton32(state)) {
-    case _STENO:
-        writePinLow(B2);
-        writePinHigh(B3);
-        writePinHigh(B6);
-        break;
     case _GAME:
         writePinHigh(B2);
         writePinLow(B3);
@@ -126,10 +120,6 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     return true;
 }
 
-void matrix_init_user() {
-    steno_set_mode(STENO_MODE_GEMINI);
-}
-
 #define DEL MT(MOD_LGUI, KC_DEL)
 #define TAB MT(MOD_LCTL, KC_TAB)
 #define ESC LT(_SYMBOL2, KC_ESC)
@@ -144,12 +134,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		KC_Z,                    KC_X,         KC_C,           KC_V,         KC_B,         KC_N,          KC_M,         KC_COMM,      KC_DOT,     KC_SLSH,
 		DEL,                     TAB,          ESC,            ENT,          SPC,          BKSP),
 
-    [_STENO] = KEYMAP(
-        STN_N1,                  STN_N2,       STN_N3,         STN_N4,       STN_N5,       STN_N6,        STN_N7,       STN_N8,       STN_N9,     STN_NA,
-		STN_S1,                  STN_TL,       STN_PL,         STN_HL,       STN_ST1,      STN_FR,        STN_PR,       STN_LR,       STN_TR,     STN_DR,
-		STN_S2,                  STN_KL,       STN_WL,         STN_RL,       STN_ST2,      STN_RR,        STN_BR,       STN_GR,       STN_SR,     STN_ZR,
-		STN_A,                   STN_O,        MO(_SYMBOL1),   STN_E,        STN_U,        XXXXXXX),
-
     [_GAME] = KEYMAP(
 		KC_Q,                    KC_W,         KC_E,           KC_R,         KC_T,         KC_Y,          KC_U,         KC_I,         KC_O,       KC_P,
 		KC_A,                    KC_S,         KC_D,           KC_F,         KC_G,         KC_H,          KC_J,         KC_K,         KC_L,       KC_SCLN,
@@ -160,12 +144,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         LGUI(KC_1),              LGUI(KC_2),   LGUI(KC_3),     LGUI(KC_4),   LGUI(KC_5),   LGUI(KC_6),    LGUI(KC_7),   LGUI(KC_8),   KC_F11,     KC_F12,
         KC_1,                    KC_2,         KC_3,           KC_4,         KC_5,         KC_6,          KC_7,         KC_8,         KC_9,       KC_0,
 		KC_F1,                   KC_F2,        KC_F3,          KC_F4,        KC_F5,        KC_F6,         KC_F7,        KC_F8,        KC_F9,      KC_F10,
-		_______,                 _______,      MO(_FUNC),        _______,    _______,      TG(_STENO)),
+		_______,                 _______,      MO(_FUNC),      _______,      _______,      _______),
 
 	[_SYMBOL2] = KEYMAP(
-        KC_EXLM,                 KC_AT,        KC_CIRC,        KC_DLR,       XXXXXXX,      XXXXXXX,       KC_LBRC,      KC_RBRC,      KC_LCBR,    KC_RCBR,
-        KC_TILD,                 KC_GRV,       KC_AMPR,        KC_PIPE,      KC_PLUS,      KC_EQL,        KC_LPRN,      KC_RPRN,      KC_UNDS,    KC_COLN,
-        KC_DQUO,                 KC_QUOT,      KC_HASH,        KC_ASTR,      KC_PERC,      KC_BSLS,       KC_MINS,      KC_LT,        KC_GT,      KC_QUES,
+        KC_TILD,                 KC_GRV,       KC_CIRC,        KC_DLR,       XXXXXXX,      XXXXXXX,       KC_LBRC,      KC_RBRC,      KC_LCBR,    KC_RCBR,
+        KC_DQUO,                 KC_QUOT,      KC_AMPR,        KC_PIPE,      KC_PLUS,      KC_EQL,        KC_LPRN,      KC_RPRN,      KC_UNDS,    KC_COLN,
+        KC_EXLM,                 KC_AT,        KC_HASH,        KC_ASTR,      KC_PERC,      KC_BSLS,       KC_MINS,      KC_LT,        KC_GT,      KC_QUES,
         _______,                 _______,      _______,      MO(_FUNC),      _______,      _______),
 
 	[_FUNC] = KEYMAP(
