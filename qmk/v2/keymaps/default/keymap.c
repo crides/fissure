ab341e91956854a800850047086000a74d1dbd07
crides <zhuhaoqing@live.cn>
2020-07-12 18:53:50 -0500

    Reorg keymap

--- qmk/v2/keymaps/default/keymap.c
+++ qmk/v2/keymaps/default/keymap.c
@@ -6,7 +6,6 @@
 #define _BASE   0
 #define _LOWER 1
 #define _RAISE 2
-#define _ADJUST 3
 
 #define KEYMAP( \
     L10, L11, L12, L13, L14, 				R10, R11, R12, R13, R14, \
@@ -46,16 +45,12 @@ enum keycodes {
 };
 
 layer_state_t layer_state_set_user(layer_state_t state) {
-    state = update_tri_layer_state(state, _LOWER, _RAISE, _ADJUST);
     switch (biton32(state)) {
     case _LOWER:
         set_hsv(HSV_GREEN);
         break;
     case _RAISE:
         set_hsv(HSV_CYAN);
-        break;
-    case _ADJUST:
-        set_hsv(HSV_MAGENTA);
         break;
     default: //  for any other layers, or the default layer
         set_hsv(0, 0, 0);
@@ -147,26 +142,20 @@ combo_t key_combos[COMBO_COUNT] = {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_BASE] = KEYMAP(
-		KC_Q,                    KC_W,         KC_E,           KC_R,         KC_T,         KC_Y,          KC_U,         KC_I,         KC_O,       KC_P,
-		KC_A,                    KC_S,         KC_D,           KC_F,         KC_G,         KC_H,          KC_J,         KC_K,         KC_L,       KC_SCLN,
-		KC_Z,                    KC_X,         KC_C,           KC_V,         KC_B,         KC_N,          KC_M,         KC_COMM,      KC_DOT,     KC_SLSH,
-		DEL,                     TAB,          ESC,            ENT,          SPC,          BKSP),
+		KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,      KC_Y,     KC_U,        KC_I,       KC_O,        KC_P,
+		KC_A,     KC_S,     KC_D,     KC_F,     KC_G,      KC_H,     KC_J,        KC_K,       KC_L,     KC_SCLN,
+		KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,      KC_N,     KC_M,     KC_COMM,     KC_DOT,     KC_SLSH,
+		 DEL,      TAB,      ESC,      ENT,      SPC,     BKSP),
 
 	[_LOWER] = KEYMAP(
-        LGUI(KC_1),              LGUI(KC_2),   LGUI(KC_3),     LGUI(KC_4),   LGUI(KC_5),   LGUI(KC_6),    LGUI(KC_7),   LGUI(KC_8),   KC_F11,     KC_F12,
-        KC_1,                    KC_2,         KC_3,           KC_4,         KC_5,         KC_6,          KC_7,         KC_8,         KC_9,       KC_0,
-		KC_F1,                   KC_F2,        KC_F3,          KC_F4,        KC_F5,        KC_F6,         KC_F7,        KC_F8,        KC_F9,      KC_F10,
-		_______,                 _______,      _______,        _______,    _______,      _______),
+        G(KC_1),   G(KC_2),   G(KC_3),   G(KC_4),   G(KC_5),    G(KC_6),   G(KC_7),   G(KC_8),   KC_F11,   KC_F12,
+           KC_1,      KC_2,      KC_3,      KC_4,      KC_5,       KC_6,      KC_7,      KC_8,     KC_9,     KC_0,
+		  KC_F1,     KC_F2,     KC_F3,     KC_F4,     KC_F5,      KC_F6,     KC_F7,     KC_F8,    KC_F9,   KC_F10,
+		_______,   _______,   _______,   _______,   _______,   _______),
 
 	[_RAISE] = KEYMAP(
-        KC_TILD,                 KC_GRV,       KC_CIRC,        KC_DLR,       KC_PERC,      KC_BSLS,       KC_LBRC,      KC_RBRC,      KC_LCBR,    KC_RCBR,
-        KC_DQUO,                 KC_QUOT,      KC_AMPR,        KC_PIPE,      KC_PLUS,      KC_EQL,        KC_LPRN,      KC_RPRN,      KC_UNDS,    KC_COLN,
-        KC_EXLM,                 KC_AT,        KC_HASH,        KC_ASTR,      XXXXXXX,      XXXXXXX,       KC_MINS,      KC_LT,        KC_GT,      KC_QUES,
-        _______,                 _______,      _______,      _______,      _______,      _______),
-
-	[_ADJUST] = KEYMAP(
-		KC_BRID,                 KC_MUTE,      KC_VOLD,        KC_VOLU,      KC_BRIU,      KC_MS_L,       KC_MS_D,      KC_MS_U,      KC_MS_R,    KC_PSCR,
-		KC_TEST,                 KC_BTN1,      KC_BTN3,        KC_BTN2,      XXXXXXX,      KC_LEFT,       KC_DOWN,      KC_UP,        KC_RIGHT,   XXXXXXX,
-		XXXXXXX,                 KC_MPRV,      KC_MPLY,        KC_MNXT,      XXXXXXX,      KC_WH_L,       KC_WH_D,      KC_WH_U,      KC_WH_R,    XXXXXXX,
-		_______,                 _______,      _______,        _______,      _______,      _______),
+		KC_BRID,  KC_MUTE,  KC_VOLD,  KC_VOLU,  KC_BRIU,     KC_HOME,     KC_PGDN,   KC_PGUP,       KC_END,  KC_PSCR,
+		XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,     KC_LEFT,     KC_DOWN,     KC_UP,     KC_RIGHT,  XXXXXXX,
+		XXXXXXX,  KC_MPRV,  KC_MPLY,  KC_MNXT,  XXXXXXX,  C(KC_LEFT),  C(KC_DOWN),  C(KC_UP),  C(KC_RIGHT),  XXXXXXX,
+		_______,  _______,  _______,  _______,  _______,    _______),
 };
