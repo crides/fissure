db4a1d6b73b3c58a3b9dff11fb613e8332db5f3c
crides <zhuhaoqing@live.cn>
2020-10-07 23:23:46 -0500

    Modify firmware to use v2.2

--- qmk/v2/config.h
+++ qmk/v2/config.h
@@ -8,10 +8,10 @@
 /* USB Device descriptor parameter */
 #define VENDOR_ID       0xFEED
 #define PRODUCT_ID      0x6060
-#define DEVICE_VER      0x0001
+#define DEVICE_VER      0x0202
 #define MANUFACTURER    Crides
-#define PRODUCT         Fissure
-#define DESCRIPTION     36 key split keyboard
+#define PRODUCT         Fissure-v2.2
+#define DESCRIPTION     40 key split keyboard
 
 #ifdef __AVR__
 #define USE_I2C
@@ -22,9 +22,9 @@
 
 #define RGBLED_NUM 2
 #ifdef __AVR__
-#define RGB_DI_PIN B5
+#define RGB_DI_PIN F7
 #else
-#define RGB_DI_PIN 38
+#define RGB_DI_PIN 9
 #endif
 #define RGBLED_SPLIT {1, 1}
 #define RGBLIGHT_SPLIT
@@ -33,11 +33,9 @@
 #define MATRIX_ROWS 8
 #define MATRIX_COLS 5
 
-/* #define MATRIX_ROW_PINS { F4, F5, F6, F7 } */
-/* #define MATRIX_COL_PINS { D4, C6, D7, E6, B4 } */
 #ifdef __AVR__
-#define MATRIX_ROW_PINS { D4, C6, D7, E6 }
-#define MATRIX_COL_PINS { D3, D2, B1, B3, B2 }
+#define MATRIX_ROW_PINS { E6, D7, C6, D4 }
+#define MATRIX_COL_PINS { D3, D2, F4, F5, F6 }
 #else
 #define BLE_NUS_MIN_INTERVAL 10 // default 60
 #define BLE_NUS_MAX_INTERVAL 30 // default 75
--- qmk/v2/keymaps/default/keymap.c
+++ qmk/v2/keymaps/default/keymap.c
@@ -12,38 +12,37 @@
 #define _RAISE 2
 
 #define KEYMAP( \
-    L10, L11, L12, L13, L14,                 R10, R11, R12, R13, R14, \
-    L20, L21, L22, L23, L24,                 R20, R21, R22, R23, R24, \
-    L30, L31, L32, L33, L34,                 R30, R31, R32, R33, R34, \
-                         L42, L43, L44,             R40, R41, R42 \
+    L40, L10, L11, L12, L13, L14,                 R10, R11, R12, R13, R14, R44, \
+    L41, L20, L21, L22, L23, L24,                 R20, R21, R22, R23, R24, R43, \
+         L30, L31, L32, L33, L34,                 R30, R31, R32, R33, R34, \
+                        L42, L43, L44,            R40, R41, R42 \
     ) \
     { \
         { L10, L11, L12, L13, L14 }, \
         { L20, L21, L22, L23, L24 }, \
         { L30, L31, L32, L33, L34 }, \
-        { XXXXXXX, XXXXXXX, L42, L43, L44 }, \
+        { L40, L41, L42, L43, L44 }, \
         { R14, R13, R12, R11, R10 }, \
         { R24, R23, R22, R21, R20 }, \
         { R34, R33, R32, R31, R30 }, \
-        { XXXXXXX, XXXXXXX, R42, R41, R40 } \
+        { R44, R43, R42, R41, R40 } \
     }
 
-#ifdef __AVR__
+/* #ifdef __AVR__ */
 void set_hsv(uint8_t a, uint8_t b, uint8_t c) {
     rgblight_sethsv(a, b, (uint8_t) c * 0.2);
 }
-#endif
+/* #endif */
 
 void keyboard_pre_init_user(void) {
-#ifdef __AVR__
     set_hsv(HSV_TEAL);
+#ifdef __AVR__
 #else
     set_usb_enabled(true);
     debug_matrix=true;
 #endif
 }
 
-#ifdef __AVR__
 void suspend_power_down_user() {
     set_hsv(0, 0, 0);
 }
@@ -52,7 +51,7 @@ void suspend_wakeup_init_user() {
     set_hsv(0, 0, 0);
 }
 
-layer_state_t layer_state_set_user(layer_state_t state) {
+uint32_t layer_state_set_user(uint32_t state) {
     switch (biton32(state)) {
     case _LOWER:
         set_hsv(HSV_GREEN);
@@ -66,6 +65,7 @@ layer_state_t layer_state_set_user(layer_state_t state) {
     }
     return state;
 }
+#ifdef __AVR__
 #else
 // Aliases
 #define C(kc) LCTL(kc)
@@ -94,10 +94,10 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_BASE] = KEYMAP(
-        KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,      KC_Y,     KC_U,        KC_I,       KC_O,        KC_P,
-        KC_A,     KC_S,     KC_D,     KC_F,     KC_G,      KC_H,     KC_J,        KC_K,       KC_L,     KC_SCLN,
-        KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,      KC_N,     KC_M,     KC_COMM,     KC_DOT,     KC_SLSH,
-         DEL,      TAB,      ESC,      ENT,      SPC,     BKSP),
+        XXXXXXX, KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,      KC_Y,     KC_U,        KC_I,       KC_O,        KC_P, KC_2,
+        XXXXXXX, KC_A,     KC_S,     KC_D,     KC_F,     KC_G,      KC_H,     KC_J,        KC_K,       KC_L,     KC_SCLN, KC_1,
+                 KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,      KC_N,     KC_M,     KC_COMM,     KC_DOT,     KC_SLSH,
+                  DEL,      TAB,      ESC,      ENT,      SPC,     BKSP),
 
     /* [_BASE] = KEYMAP( */
     /*     KC_Q,  KC_W,  KC_F,  KC_P,  KC_B,   KC_J,  KC_L,     KC_U,    KC_Y,  KC_SCLN, */
@@ -106,16 +106,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /*      DEL,   TAB,   ESC,   ENT,   SPC,  BKSP), */
 
     [_LOWER] = KEYMAP(
-        G(KC_1),  G(KC_2),  G(KC_3),  G(KC_4),  G(KC_5),   G(KC_6),  G(KC_7),  G(KC_8),  KC_F11,  KC_F12,
-           KC_1,     KC_2,     KC_3,     KC_4,     KC_5,      KC_6,     KC_7,     KC_8,    KC_9,    KC_0,
-          KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,     KC_F6,    KC_F7,    KC_F8,   KC_F9,  KC_F10,
-        _______,  _______,  _______,  _______,  _______,  _______),
+        XXXXXXX, G(KC_1),  G(KC_2),  G(KC_3),  G(KC_4),  G(KC_5),   G(KC_6),  G(KC_7),  G(KC_8),  KC_F11,  KC_F12, XXXXXXX,
+        XXXXXXX,    KC_1,     KC_2,     KC_3,     KC_4,     KC_5,      KC_6,     KC_7,     KC_8,    KC_9,    KC_0, XXXXXXX,
+                   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,     KC_F6,    KC_F7,    KC_F8,   KC_F9,  KC_F10,
+                 _______,  _______,  _______,  _______,  _______,  _______),
 
     [_RAISE] = KEYMAP(
-        KC_BRID,  KC_MUTE,  KC_VOLD,  KC_VOLU,  KC_BRIU,     KC_HOME,     KC_PGDN,   KC_PGUP,       KC_END,  KC_PSCR,
-        XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,     KC_LEFT,     KC_DOWN,     KC_UP,     KC_RIGHT,  XXXXXXX,
-        XXXXXXX,  KC_MPRV,  KC_MPLY,  KC_MNXT,  XXXXXXX,  C(KC_LEFT),  C(KC_DOWN),  C(KC_UP),  C(KC_RIGHT),  XXXXXXX,
-        _______,  _______,  _______,  _______,  _______,    _______),
+        XXXXXXX, KC_BRID,  KC_MUTE,  KC_VOLD,  KC_VOLU,  KC_BRIU,     KC_HOME,     KC_PGDN,   KC_PGUP,       KC_END,  KC_PSCR, XXXXXXX,
+        XXXXXXX, XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,     KC_LEFT,     KC_DOWN,     KC_UP,     KC_RIGHT,  XXXXXXX, XXXXXXX,
+                 XXXXXXX,  KC_MPRV,  KC_MPLY,  KC_MNXT,  XXXXXXX,  C(KC_LEFT),  C(KC_DOWN),  C(KC_UP),  C(KC_RIGHT),  XXXXXXX,
+                 _______,  _______,  _______,  _______,  _______,    _______),
 };
 
 const uint16_t PROGMEM combos[COMBO_COUNT][3] = {
